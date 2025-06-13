e7d575f948ab5430c49d75be4d65584abf64fbcd
Samuel <sami6543pz@gmail.com>
2025-06-13 16:13:04 -0500

    Added functionality to panel that appears after cliking one of the options, saying whether you got it right or wrong.

--- quiz app/quiz_app.js
+++ quiz app/quiz_app.js
@@ -11,17 +11,69 @@ const questions = [
     option3: "c) 0",
     option4: "d) 4",
     answer: "d) 4",
+    explanation: "2+2 = 4 because two adds to two, giving four.",
   }
 ];
 
 const container = document.getElementById('container');
+const questionText = document.getElementById('question-text');
+const buttons = document.querySelectorAll('.btn');
+const answerBox = document.querySelector('.answer-box');
+const explanation = document.getElementById('explanation');
+const resultBtn = document.getElementById('result-btn');
+const isCorrectH1 = document.getElementById('is-correct');
+const explanationText = document.getElementById('explanation-text');
+const continueBtn = document.getElementById('continue');
 
-container.innerHTML = `
-  <div class="q-section"><h1 class="question">${questions[0].question}</h1></div>
-  <div class="options">
-    <button class="btn">${questions[0].option1}</button>
-    <button class="btn">${questions[0].option2}</button>
-    <button class="btn">${questions[0].option3}</button>
-    <button class="btn">${questions[0].option4}</button>
-  </div>
-`;
+let currentQuestionIndex = 0;
+updateQuiz();
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    answerBox.classList.toggle('hidden');
+    if (checkIfCorrectAnswer(btn)) {
+      updateAnswerBox("right", questions[currentQuestionIndex][`${btn.id}`])
+    } else {
+      updateAnswerBox("wrong", questions[currentQuestionIndex][`${btn.id}`])
+    }
+
+  });
+});
+
+
+
+function updateQuiz() {
+  questionText.innerText = questions[currentQuestionIndex].question;
+  buttons.forEach(btn => {
+    btn.innerText = questions[currentQuestionIndex][`${btn.id}`];
+  });
+
+}
+
+function updateAnswerBox(correctness, optionText) {
+  if (correctness == "right") {
+    resultBtn.classList.add('right');
+    resultBtn.classList.remove('wrong');
+
+    isCorrectH1.classList.add('right');
+    isCorrectH1.classList.remove('wrong');
+    isCorrectH1.innerText = "is RIGHT";
+  } else {
+    resultBtn.classList.add('wrong');
+    resultBtn.classList.remove('right');
+
+    isCorrectH1.classList.add('wrong');
+    isCorrectH1.classList.remove('right');
+    isCorrectH1.innerText = "is WRONG";
+  }
+  resultBtn.innerText = optionText;
+  explanationText.innerText = questions[currentQuestionIndex].explanation;
+}
+
+function checkIfCorrectAnswer(e) {
+  if (e.innerText == questions[currentQuestionIndex].answer) {
+    return true;
+  } else {
+    return false;
+  }
+}
