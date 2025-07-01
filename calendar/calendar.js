dabd1c8f3c8168fe5516b66ebaba6b3432e395cf
Samuel <sami6543pz@gmail.com>
2025-07-01 17:40:07 -0500

    Created function to update calendar to show the days of the month in Javascript

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,11 +1,41 @@
 const daysContainer = document.getElementById('days-container');
+const monthYearText = document.getElementById('month-year-text');
 
-const date = new Date();
-const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
-const daysInCurrentMonth = daysInMonth(date.getFullYear(), date.getMonth() + 1);
 
-for (let i = 1; i <= daysInCurrentMonth; i++) {
-  daysContainer.innerHTML += `
-  <div class="day">${i}</div>
-  `;
+let currentDate = new Date();
+
+function updateCalendar() {
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+
+  const firstDay = new Date(currentYear, currentMonth, 0);
+  const lastDay = new Date(currentYear, currentMonth + 1, 0);
+  const totalDays = lastDay.getDate();
+  const firstDayIndex = firstDay.getDay();
+  const lastDayIndex = lastDay.getDay();
+
+  const monthYearString = currentDate.toLocaleString('default', 
+    {month: 'long', year: 'numeric'});
+    monthYearText.textContent = monthYearString;
+
+  //adds previous days to match weekday row
+  for (let i = firstDayIndex; i > 0; i--) {
+    const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+    daysContainer.innerHTML += `<div class="day inactive">${prevDate.getDate()}</div>`;
+  }
+  //adds days for the month matching the weekday row
+  for (let i = 1; i <= totalDays; i++) {
+    const date = new Date(currentYear, currentMonth, i);
+    const activeClass = date.toDateString() === new Date().toDateString()
+    ? 'active' : '';
+    daysContainer.innerHTML += `<div class="day ${activeClass}">${i}</div>`;
+
+  }
+  //adds days after the month matching the weekday row
+  for (let i = 1; i <= 7 - lastDayIndex; i++) {
+    const nextDate = new Date(currentYear, currentMonth + 1, i);
+    daysContainer.innerHTML += `<div class="day inactive">${nextDate.getDate()}</div>`;
+  }
 }
+
+updateCalendar();
