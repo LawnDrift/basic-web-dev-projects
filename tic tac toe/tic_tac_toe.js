6edf139b42c41b2b7cd351a66bc497d939b95960
Samuel <sami6543pz@gmail.com>
2025-07-12 16:32:29 -0500

    Got minimax AI working in the console.

--- tic tac toe/tic_tac_toe.js
+++ tic tac toe/tic_tac_toe.js
@@ -1,21 +1,87 @@
 const board = [
-  ["X", "O", ""],
-  ["X", "O", "O"],
-  ["O", "", ""]
+  [" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "]
 ];
 
-let openSpots = 9;
+
 let human = "X";
 let ai = "O";
 let currentPlayer = human;
 
-console.log(board[0][0]);
-console.log(board[1][0]);
-console.log(board[2][0]);
-console.log(checkWinner() + " wins");
+let scores = {
+  'X': -1,
+  'O': 1,
+  'tie': 0
+};
+
+console.log("Welcome to Tic Tac Toe Game");
+
+drawBoard();
+
+while (true) {
+  if (currentPlayer == human) {
+    let rowResponse = prompt("Please enter an integer between 0 and 2 as row:");
+    let columnResponse = prompt("Please enter an integer between 0 and 2 as column:");
+    let rowInt = parseInt(rowResponse, 10);
+    let columnInt = parseInt(columnResponse, 10);
+    if (isNaN(rowInt) || isNaN(columnInt)) {
+      console.log("Invalid Input, try again.");
+      continue;
+    } 
+    else if ((rowInt > 2 || rowInt < 0) || (columnInt > 2 || columnInt < 0)) {
+      console.log("The integer value must be between 0 and 2 (0, 1, 2).")
+      continue;
+    }
+    
+    else {
+      if (board[rowInt][columnInt] !== " ") {
+        continue;
+      }
+      board[rowInt][columnInt] = human;
+  
+      if (checkWinner() !== null) {
+        drawBoard();
+        console.log(`${checkWinner() !== "tie" ?
+           checkWinner() + "wins the game!" : "Tie!!!"}`);
+
+        break;
+      }
+      currentPlayer = ai;
+      bestMove();
+      
+    }
+    drawBoard();
+    if (checkWinner() !== null) {
+      console.log(`${checkWinner()} wins the game!`);
+      break;
+    }
+  }
 
 
 
+   
+}
+
+function drawBoard() {
+  console.log(`
+      |     |  |     |  |     |
+      |  ${board[0][0]}  |  |  ${board[0][1]}  |  |  ${board[0][2]}  |
+      |     |  |     |  |     |
+      -------------------------
+      |     |  |     |  |     |
+      |  ${board[1][0]}  |  |  ${board[1][1]}  |  |  ${board[1][2]}  |
+      |     |  |     |  |     |
+      -------------------------
+      |     |  |     |  |     |
+      |  ${board[2][0]}  |  |  ${board[2][1]}  |  |  ${board[2][2]}  |
+      |     |  |     |  |     |
+      -------------------------
+              
+    
+    `);
+  
+}
 
 
 function bestMove() {
@@ -25,13 +91,13 @@ function bestMove() {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       //Is the spot available?
-      if (board[i][j] == "") {
+      if (board[i][j] == " ") {
         board[i][j] = ai;
-        let score = minimax(board, 0, true);
-        board[i][j] = '';
+        let score = minimax(board, 0, false);
+        board[i][j] = ' ';
         if (score > bestScore) {
           bestScore = score;
-          move = {i, j};
+          move = { i, j };
         }
       }
     }
@@ -40,17 +106,12 @@ function bestMove() {
   currentPlayer = human;
 }
 
-let scores = {
-  'X': -1,
-  'O': 1,
-  'tie': 0
-};
+
 
 function minimax(board, depth, isMaximizing) {
   let result = checkWinner();
   if (result !== null) {
-    let score = scores[result];
-    return true;
+    return scores[result];
   }
 
   if (isMaximizing) {
@@ -58,57 +119,72 @@ function minimax(board, depth, isMaximizing) {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         // Is the spot available?
-        if (board[i][j] == '') {
+        if (board[i][j] == " ") {
           board[i][j] = ai;
           let score = minimax(board, depth + 1, false);
-          board[i][j] = '';
-          bestScore = max(score, bestScore);
+          board[i][j] = " ";
+          bestScore = Math.max(score, bestScore);
         }
       }
     }
     return bestScore;
-
   } else {
     let bestScore = Infinity;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         // Is the spot available?
-        if (board[i][j] == '') {
+        if (board[i][j] == " ") {
           board[i][j] = human;
           let score = minimax(board, depth + 1, true);
-          board[i][j] = '';
-          bestScore = min(score, bestScore);
+          board[i][j] = " ";
+          bestScore = Math.min(score, bestScore);
         }
       }
     }
     return bestScore;
   }
-
-
 }
+
 
 function checkWinner() {
   let winner = null;
   for (let i = 0; i < 3; i++) {
+    
     //check if a row has 3 of the same symbol (O or X)
-    if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+    if (board[i][0] == board[i][1] && board[i][1] == board[i][2] 
+      && (board[i][0] == "O" || board[i][0] == "X")
+    ) {
       winner = board[i][0]; //either X or O
     }
     //check if a column has 3 of the same symbol (O or X)
-    if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+    if (board[0][i] == board[1][i] && board[1][i] == board[2][i] 
+      && (board[0][i] == "O" || board[0][i] == "X")) {
       winner = board[0][i]; //either X or O
     }
     //both if statements check if the diagonals have three X or O
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] 
+      && (board[0][0] == "O" ||board[0][0] == "X")) {
       winner = board[0][0]; //either X or O
     }
-    if (board[2][0] == board[1][1] && board[1][1] == board[0][2]) {
+    if (board[2][0] == board[1][1] && board[1][1] == board[0][2] 
+      && (board[2][0] == "O" || board[2][0] == "X")) {
       winner = board[2][0]; //either X or O
     }
   }
 
-  if (winner == null && openSpots == 0) {
-        return 'tie';
+  let openSpots = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] == ' ') {
+        openSpots++;
       }
-      return winner;
+    }
+  }
+
+  if (winner == null && openSpots == 0) {
+    return 'tie';
+  } else {
+    return winner;
+  }
+  
 }
